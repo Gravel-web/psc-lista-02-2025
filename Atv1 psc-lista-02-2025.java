d80b0735b2f50ad8611b8da5d6cfda1debc5a0c9
Felipe Gravel <gravelfelipe@gmail.com>
2025-03-16 19:05:31 -0300

    psc-lista-02-2025
    Atividade prática em Java, Programação de Soluções Computacionais.

---
+++ Atv1 psc-lista-02-2025.java
@@ -0,0 +1,12 @@
+// Atividade 1, lista 2- Programação de Soluções Computacionais
+import java.util.Scanner;
+public class prt1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int n1 = sc.nextInt();
+        System.out.println("O número informado foi " + n1);
+        sc.close();
+            }
+        
+        }
